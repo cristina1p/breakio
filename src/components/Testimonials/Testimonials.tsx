91094af8c416adc0a4cbbd3fb1fc7c77a87cf70c
cristina <c1plamadeala@gmail.com>
2026-05-04 12:02:31 +0300

    Implement carousel with index based navigation

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -5,9 +5,16 @@ import typo from "../../styles/typography.module.css";
 import module from "../../styles/layout.module.css";
 import shapeLeft from "../../assets/testimonials_images/deco/shapeLeft.png";
 import shapeRight from "../../assets/testimonials_images/deco/shapeRight.png";
+import { ArrowButton } from "../Stories/ArrowButton";
+import { useState } from "react";
 
 export function Testimonials() {
-  const firstEntry = testimonialData[0];
+  const [index, setIndex] = useState(0);
+
+  const handlePrev = () => setIndex((prev) => Math.max(0, prev - 1));
+  const handleNext = () =>
+    setIndex((prev) => Math.min(testimonialData.length - 1, prev + 1));
+
   return (
     <section
       className={`${styles.testimonialContainer} ${module.sectionContent}`}
@@ -18,7 +25,21 @@ export function Testimonials() {
       <h2 className={`${module.title} ${typo.titleSection}`}>
         What other users say
       </h2>
-      <TestimonialCard testimonial={firstEntry} />
+      <div className={styles.testimonialWrapper}>
+        <ArrowButton
+          direction="left"
+          onClick={handlePrev}
+          disabled={index === 0}
+          className={`${styles.leftButton}`}
+        />
+        <ArrowButton
+          direction="right"
+          onClick={handleNext}
+          disabled={index === testimonialData.length - 1}
+          className={`${styles.rightButton}`}
+        />
+        <TestimonialCard testimonial={testimonialData[index]} />
+      </div>
     </section>
   );
 }
